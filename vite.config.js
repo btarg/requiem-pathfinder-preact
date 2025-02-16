69ef2a986ab92bd97365060a61d27df7e7bc8ff9
btarg <27726289+btarg@users.noreply.github.com>
2025-02-16 14:11:34 +0000

    SPA 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { resolve } from 'path';
+import { writeFileSync, readFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-	base: '/requiem-pathfinder-preact/',
+    plugins: [
+        preact(),
+        {
+            name: 'copy-404',
+            writeBundle() {
+                writeFileSync(
+                    resolve(__dirname, 'dist/404.html'),
+                    readFileSync(resolve(__dirname, 'public/404.html'))
+                );
+            }
+        }
+    ],
+    base: '/requiem-pathfinder-preact/',
 });
